c900ab3e2546877aae1a6d0c9a4ba61e2208a762
Alexandr Bachurin <pplazy1982@gmail.com>
2021-11-28 07:41:12 +0300

    4.9 80% about language

--- src/components/r-MainStatus.vue
+++ src/components/r-MainStatus.vue
@@ -3,7 +3,12 @@
     <div class="stats">
       <div class="start startHp">
         <div class="start__stats">
-          <input v-model="$store.state.options[109]" type="number" class="stats_input startHp_input" placeholder="Start HP" /><output class="stats_output startHp_output">+{{ bonusHp }}</output>
+          <input
+            v-model="$store.state.options[109]"
+            type="number"
+            class="stats_input startHp_input"
+            placeholder="Start HP"
+          /><output class="stats_output startHp_output">+{{ bonusHp }}</output>
         </div>
         <div class="start__res">
           <span>{{ $store.state.lang.hp }}-</span>
@@ -13,7 +18,12 @@
 
       <div class="start startAtk">
         <div class="start__stats">
-          <input v-model="$store.state.options[110]" type="number" class="stats_input" placeholder="Start ATK" /><output class="stats_output">+{{ bonusAtk }}</output>
+          <input
+            v-model="$store.state.options[110]"
+            type="number"
+            class="stats_input"
+            placeholder="Start ATK"
+          /><output class="stats_output">+{{ bonusAtk }}</output>
         </div>
         <div class="start__res">
           <span>{{ $store.state.lang.atk }}-</span>
@@ -23,7 +33,12 @@
 
       <div class="start startDef">
         <div class="start__stats">
-          <input v-model="$store.state.options[111]" type="number" class="stats_input" placeholder="Start DEF" /><output class="stats_output">+{{ bonusDef }}</output>
+          <input
+            v-model="$store.state.options[111]"
+            type="number"
+            class="stats_input"
+            placeholder="Start DEF"
+          /><output class="stats_output">+{{ bonusDef }}</output>
         </div>
         <div class="start__res">
           <span>{{ $store.state.lang.def }}-</span>
@@ -33,17 +48,31 @@
 
       <div class="start startSpeed">
         <div class="start__stats">
-          <input v-model="$store.state.options[112]" type="number" class="stats_input" placeholder="Start SPD" /><output class="stats_output">+{{ bonusSpd }}</output>
+          <input
+            v-model="$store.state.options[112]"
+            type="number"
+            class="stats_input"
+            placeholder="Start SPD"
+          /><output class="stats_output">+{{ bonusSpd }}</output>
         </div>
         <div class="start__res">
           <span>{{ $store.state.lang.spd }}-</span>
-          <p>{{ Math.round((+$store.state.options[112] + +bonusSpd) * 100) / 100 }}</p>
+          <p>
+            {{
+              Math.round((+$store.state.options[112] + +bonusSpd) * 100) / 100
+            }}
+          </p>
         </div>
       </div>
 
       <div class="start startCrate">
         <div class="start__stats">
-          <input v-model="$store.state.options[113]" type="number" class="stats_input" placeholder="Start C.Rate" /><output class="stats_output">+{{ bonusCrate }}%</output>
+          <input
+            v-model="$store.state.options[113]"
+            type="number"
+            class="stats_input"
+            placeholder="Start C.Rate"
+          /><output class="stats_output">+{{ bonusCrate }}%</output>
         </div>
         <div class="start__res">
           <span>{{ $store.state.lang.minCritRate }}-</span>
@@ -53,7 +82,12 @@
 
       <div class="start startCdmg">
         <div class="start__stats">
-          <input v-model="$store.state.options[114]" type="number" class="stats_input" placeholder="Start C.Dmg" /><output class="stats_output">+{{ bonusCdmg }}%</output>
+          <input
+            v-model="$store.state.options[114]"
+            type="number"
+            class="stats_input"
+            placeholder="Start C.Dmg"
+          /><output class="stats_output">+{{ bonusCdmg }}%</output>
         </div>
         <div class="start__res">
           <span>{{ $store.state.lang.minCritDamage }}-</span>
@@ -63,7 +97,12 @@
 
       <div class="start startResist">
         <div class="start__stats">
-          <input v-model="$store.state.options[115]" type="number" class="stats_input" placeholder="Start RES" /><output class="stats_output">+{{ bonusResist }}</output>
+          <input
+            v-model="$store.state.options[115]"
+            type="number"
+            class="stats_input"
+            placeholder="Start RES"
+          /><output class="stats_output">+{{ bonusResist }}</output>
         </div>
         <div class="start__res">
           <span>{{ $store.state.lang.resMin }}-</span>
@@ -73,7 +112,12 @@
 
       <div class="start startAcc">
         <div class="start__stats">
-          <input v-model="$store.state.options[116]" type="number" class="stats_input" placeholder="Start ACC" /><output class="stats_output">+{{ bonusAcc }}</output>
+          <input
+            v-model="$store.state.options[116]"
+            type="number"
+            class="stats_input"
+            placeholder="Start ACC"
+          /><output class="stats_output">+{{ bonusAcc }}</output>
         </div>
         <div class="start__res">
           <span>{{ $store.state.lang.acc }}-</span>
@@ -109,7 +153,14 @@ export default {
     options() {
       let options = this.$store.state.options;
 
-      let set = [options[0], options[11], options[22], options[33], options[44], options[55]]
+      let set = [
+        options[0],
+        options[11],
+        options[22],
+        options[33],
+        options[44],
+        options[55],
+      ]
         .sort()
         .filter((str) => str.length > 10)
         .filter((item, i, arr) => item === arr[i - 1] || item === arr[i + 1]);
@@ -123,7 +174,24 @@ export default {
         return names[str];
       }
       //========================================Массив с "0"-ми,для исключения ошибок при расчетах
-      let resultSet = ["hp_100", "0", "atk_100", "0", "def_100", "0", "spd_100", "0", "cRate", "0", "cDmg", "0", "resist", "0", "acc", "0"];
+      let resultSet = [
+        "hp_100",
+        "0",
+        "atk_100",
+        "0",
+        "def_100",
+        "0",
+        "spd_100",
+        "0",
+        "cRate",
+        "0",
+        "cDmg",
+        "0",
+        "resist",
+        "0",
+        "acc",
+        "0",
+      ];
       //========================================Добавляем только полные сеты
       for (let i = 0; i < set.length; i += count(set[i])) {
         switch (Math.floor(count(set[i]) / set[i][3])) {
@@ -144,31 +212,300 @@ export default {
       //==================================Преобразуем в нужную форму
       resultSet = resultSet.join().split(",");
       //==================================Расчеты
+      let powerHp = 0;
+      let powerAtk = 0;
+      let powerDef = 0;
+      let powerResist = 0;
+      let powerAcc = 0;
+      let powerSpd = 0;
+      let powerSpdLeg = 0;
+      let powerCrateLeg = 0;
+      let powerCdmgLeg = 0;
+      let powerResistLeg = 0;
+      let powerAccLeg = 0;
+
+      switch (options[118]) {
+        case "1":
+          powerResistLeg = 15;
+          powerAccLeg = 15;
+          break;
+        case "2":
+          powerSpdLeg = 10;
+          powerResistLeg = 25;
+          powerAccLeg = 25;
+          break;
+        case "3":
+          powerSpdLeg = 10;
+          powerResistLeg = 45;
+          powerAccLeg = 45;
+          break;
+        case "4":
+          powerSpdLeg = 15;
+          powerCrateLeg = 10;
+          powerCdmgLeg = 30;
+          powerResistLeg = 55;
+          powerAccLeg = 55;
+          break;
+      }
+
+      // ==========power--> Усиленние персонажей в патче 4.9
+      switch (options[117]) {
+        case "3":
+          switch (options[119]) {
+            case "10":
+              powerHp = (+options[109] / 10) * (1 + +options[118]);
+              powerAtk = (+options[110] / 10) * (1 + +options[118]);
+              powerResist = 30 + powerResistLeg;
+              powerAcc = 30 + powerAccLeg;
+              powerDef = (+options[111] / 10) * (1 + +options[118]);
+              powerSpd = 10 + powerSpdLeg;
+              break;
+            case "8":
+              powerHp = (+options[109] / 10) * (1 + +options[118]);
+              powerAtk = (+options[110] / 10) * (1 + +options[118]);
+              powerResist = 30 + powerResistLeg;
+              powerAcc = 30 + powerAccLeg;
+              powerDef = (+options[111] / 10) * (1 + +options[118]);
+              powerSpd = powerSpdLeg;
+              break;
+            case "6":
+              powerHp = (+options[109] / 10) * (1 + +options[118]);
+              powerAtk = (+options[110] / 10) * (1 + +options[118]);
+              powerResist = 30 + powerResistLeg;
+              powerAcc = 30 + powerAccLeg;
+              powerDef = (+options[111] / 10) * +options[118];
+              powerSpd = powerSpdLeg;
+              break;
+            case "4":
+              powerHp = (+options[109] / 10) * (1 + +options[118]);
+              powerAtk = (+options[110] / 10) * (1 + +options[118]);
+              powerResist = powerResistLeg;
+              powerAcc = powerAccLeg;
+              powerDef = (+options[111] / 10) * +options[118];
+              powerSpd = powerSpdLeg;
+              break;
+            case "2":
+              powerHp = (+options[109] / 10) * (1 + +options[118]);
+              powerAtk = (+options[110] / 10) * +options[118];
+              powerResist = powerResistLeg;
+              powerAcc = powerAccLeg;
+              powerDef = (+options[111] / 10) * +options[118];
+              powerSpd = powerSpdLeg;
+              break;
+          }
+
+          break;
+        case "2":
+          switch (options[119]) {
+            case "10":
+              powerHp = +options[109] / 10;
+              powerAtk = +options[110] / 10;
+              powerResist = 15;
+              powerAcc = 15;
+              powerDef = +options[111] / 10;
+              powerSpd = 6;
+              break;
+            case "8":
+              powerHp = +options[109] / 10;
+              powerAtk = +options[110] / 10;
+              powerResist = 15;
+              powerAcc = 15;
+              powerDef = +options[111] / 10;
+              break;
+            case "6":
+              powerHp = +options[109] / 10;
+              powerAtk = +options[110] / 10;
+              powerResist = 15;
+              powerAcc = 15;
+              break;
+            case "4":
+              powerHp = +options[109] / 10;
+              powerAtk = +options[110] / 10;
+              break;
+            case "2":
+              powerHp = +options[109] / 10;
+              break;
+          }
+
+          break;
+        case "1":
+          switch (options[119]) {
+            case "10":
+              powerHp = +options[109] / 10;
+              powerAtk = +options[110] / 10;
+              powerResist = 7;
+              powerAcc = 7;
+              powerDef = +options[111] / 10;
+              powerSpd = 3;
+              break;
+            case "8":
+              powerHp = +options[109] / 10;
+              powerAtk = +options[110] / 10;
+              powerResist = 7;
+              powerAcc = 7;
+              powerDef = +options[111] / 10;
+              break;
+            case "6":
+              powerHp = +options[109] / 10;
+              powerAtk = +options[110] / 10;
+              powerResist = 7;
+              powerAcc = 7;
+              break;
+            case "4":
+              powerHp = +options[109] / 10;
+              powerAtk = +options[110] / 10;
+              break;
+            case "2":
+              powerHp = +options[109] / 10;
+              break;
+          }
+
+          break;
+      }
 
       let bonusSet = options[107] === "1.15" ? 1.15 : 1;
-      let hp_sum = options.filter((item, i, arr) => arr[i - 1] === "hp_sum").reduce((a, b) => +a + +b) + (options[105] === "3" ? 810 : 0) + (options[108] === "4" ? 3000 : 0);
-      let hp_100 = options.filter((item, i, arr) => arr[i - 1] === "hp_100").reduce((a, b) => +a + +b) + +options[97] + +options[103] + resultSet.filter((item, i, arr) => arr[i - 1] === "hp_100").reduce((a, b) => +a + +b) * bonusSet;
-      let atk_sum = options.filter((item, i, arr) => arr[i - 1] === "atk_sum").reduce((a, b) => +a + +b) + (options[104] === "1" ? 75 : 0);
-      let atk_100 = options.filter((item, i, arr) => arr[i - 1] === "atk_100").reduce((a, b) => +a + +b) + +options[98] + +options[103] + resultSet.filter((item, i, arr) => arr[i - 1] === "atk_100").reduce((a, b) => +a + +b) * bonusSet;
-      let def_sum = options.filter((item, i, arr) => arr[i - 1] === "def_sum").reduce((a, b) => +a + +b) + (options[104] === "3" ? 75 : 0) + (options[105] === "1" ? 75 : 0) + (options[108] === "2" ? 200 : 0);
-      let def_100 = options.filter((item, i, arr) => arr[i - 1] === "def_100").reduce((a, b) => +a + +b) + +options[99] + +options[103] + resultSet.filter((item, i, arr) => arr[i - 1] === "def_100").reduce((a, b) => +a + +b) * bonusSet;
-      let spd = options.filter((item, i, arr) => arr[i - 1] === "spd").reduce((a, b) => +a + +b);
-      let spd_100 = resultSet.filter((item, i, arr) => arr[i - 1] === "spd_100").reduce((a, b) => +a + +b) * bonusSet;
-      let cRate = options.filter((item, i, arr) => arr[i - 1] === "cRate").reduce((a, b) => +a + +b) + (options[104] === "2" ? 5 : 0) + resultSet.filter((item, i, arr) => arr[i - 1] === "cRate").reduce((a, b) => +a + +b) * bonusSet;
-      let cDmg = options.filter((item, i, arr) => arr[i - 1] === "cDmg").reduce((a, b) => +a + +b) + (options[106] === "1" ? 10 : 0) + (options[108] === "1" ? 20 : 0) + +options[100] + resultSet.filter((item, i, arr) => arr[i - 1] === "cDmg").reduce((a, b) => +a + +b) * bonusSet;
-      let resist = options.filter((item, i, arr) => arr[i - 1] === "resist").reduce((a, b) => +a + +b) + (options[104] === "4" ? 10 : 0) + (options[105] === "2" ? 10 : 0) + (options[108] === "3" ? 50 : 0) + +options[101] + resultSet.filter((item, i, arr) => arr[i - 1] === "resist").reduce((a, b) => +a + +b) * bonusSet;
-      let acc = options.filter((item, i, arr) => arr[i - 1] === "acc").reduce((a, b) => +a + +b) + (options[105] === "4" ? 10 : 0) + (options[108] === "5" ? 50 : 0) + +options[102] + resultSet.filter((item, i, arr) => arr[i - 1] === "acc").reduce((a, b) => +a + +b) * bonusSet;
+      let hp_sum =
+        options
+          .filter((item, i, arr) => arr[i - 1] === "hp_sum")
+          .reduce((a, b) => +a + +b) +
+        (options[105] === "3" ? 810 : 0) +
+        (options[108] === "4" ? 3000 : 0);
+      let hp_100 =
+        options
+          .filter((item, i, arr) => arr[i - 1] === "hp_100")
+          .reduce((a, b) => +a + +b) +
+        +options[97] +
+        +options[103] +
+        resultSet
+          .filter((item, i, arr) => arr[i - 1] === "hp_100")
+          .reduce((a, b) => +a + +b) *
+          bonusSet;
+      let atk_sum =
+        options
+          .filter((item, i, arr) => arr[i - 1] === "atk_sum")
+          .reduce((a, b) => +a + +b) + (options[104] === "1" ? 75 : 0);
+      let atk_100 =
+        options
+          .filter((item, i, arr) => arr[i - 1] === "atk_100")
+          .reduce((a, b) => +a + +b) +
+        +options[98] +
+        +options[103] +
+        resultSet
+          .filter((item, i, arr) => arr[i - 1] === "atk_100")
+          .reduce((a, b) => +a + +b) *
+          bonusSet;
+      let def_sum =
+        options
+          .filter((item, i, arr) => arr[i - 1] === "def_sum")
+          .reduce((a, b) => +a + +b) +
+        (options[104] === "3" ? 75 : 0) +
+        (options[105] === "1" ? 75 : 0) +
+        (options[108] === "2" ? 200 : 0);
+      let def_100 =
+        options
+          .filter((item, i, arr) => arr[i - 1] === "def_100")
+          .reduce((a, b) => +a + +b) +
+        +options[99] +
+        +options[103] +
+        resultSet
+          .filter((item, i, arr) => arr[i - 1] === "def_100")
+          .reduce((a, b) => +a + +b) *
+          bonusSet;
+      let spd = options
+        .filter((item, i, arr) => arr[i - 1] === "spd")
+        .reduce((a, b) => +a + +b);
+      let spd_100 =
+        resultSet
+          .filter((item, i, arr) => arr[i - 1] === "spd_100")
+          .reduce((a, b) => +a + +b) * bonusSet;
+      let cRate =
+        options
+          .filter((item, i, arr) => arr[i - 1] === "cRate")
+          .reduce((a, b) => +a + +b) +
+        (options[104] === "2" ? 5 : 0) +
+        resultSet
+          .filter((item, i, arr) => arr[i - 1] === "cRate")
+          .reduce((a, b) => +a + +b) *
+          bonusSet;
+      let cDmg =
+        options
+          .filter((item, i, arr) => arr[i - 1] === "cDmg")
+          .reduce((a, b) => +a + +b) +
+        (options[106] === "1" ? 10 : 0) +
+        (options[108] === "1" ? 20 : 0) +
+        +options[100] +
+        resultSet
+          .filter((item, i, arr) => arr[i - 1] === "cDmg")
+          .reduce((a, b) => +a + +b) *
+          bonusSet;
+      let resist =
+        options
+          .filter((item, i, arr) => arr[i - 1] === "resist")
+          .reduce((a, b) => +a + +b) +
+        (options[104] === "4" ? 10 : 0) +
+        (options[105] === "2" ? 10 : 0) +
+        (options[108] === "3" ? 50 : 0) +
+        +options[101] +
+        resultSet
+          .filter((item, i, arr) => arr[i - 1] === "resist")
+          .reduce((a, b) => +a + +b) *
+          bonusSet;
+      let acc =
+        options
+          .filter((item, i, arr) => arr[i - 1] === "acc")
+          .reduce((a, b) => +a + +b) +
+        (options[105] === "4" ? 10 : 0) +
+        (options[108] === "5" ? 50 : 0) +
+        +options[102] +
+        resultSet
+          .filter((item, i, arr) => arr[i - 1] === "acc")
+          .reduce((a, b) => +a + +b) *
+          bonusSet;
       //=========================================Присваиваем в DOM ++ Извращение с плавающей запятой
-      this.bonusHp = Math.round(hp_sum + (hp_100 * options[109]) / 100);
-      this.bonusAtk = Math.round(atk_sum + (atk_100 * options[110]) / 100);
-      this.bonusDef = Math.round(def_sum + (def_100 * options[111]) / 100);
-      this.bonusSpd = Math.round((spd + (spd_100 * options[112]) / 100) * 100) / 100;
-      this.bonusCrate = Math.round(cRate * 100) / 100;
-      this.bonusCdmg = Math.round(cDmg * 100) / 100;
-      this.bonusResist = Math.round(resist * 100) / 100;
-      this.bonusAcc = Math.round(acc * 100) / 100;
+      this.bonusHp = Math.round(
+        hp_sum + powerHp + (hp_100 * options[109]) / 100
+      );
+      this.bonusAtk = Math.round(
+        atk_sum + powerAtk + (atk_100 * options[110]) / 100
+      );
+      this.bonusDef = Math.round(
+        def_sum + powerDef + (def_100 * options[111]) / 100
+      );
+      this.bonusSpd =
+        Math.round((spd + powerSpd + (spd_100 * options[112]) / 100) * 100) /
+        100;
+      this.bonusCrate = Math.round((cRate + powerCrateLeg) * 100) / 100;
+      this.bonusCdmg = Math.round((cDmg + powerCdmgLeg) * 100) / 100;
+      this.bonusResist = Math.round((resist + powerResist) * 100) / 100;
+      this.bonusAcc = Math.round((acc + powerAcc) * 100) / 100;
 
-      console.log(`resultSet: ${resultSet}`, bonusSet, "hp_sum = ", hp_sum, "hp_100", hp_100, "atk_sum=", atk_sum, "atk_100", atk_100, "def_sum", def_sum, "def_100", def_100, "spd", spd, "cRate", cRate, "cDmg", cDmg, "resist", resist, "acc", acc, "set");
+      console.log(
+        `resultSet: ${resultSet}`,
+        bonusSet,
+        "hp_sum = ",
+        hp_sum,
+        "hp_100",
+        hp_100,
+        "atk_sum=",
+        atk_sum,
+        "atk_100",
+        atk_100,
+        "def_sum",
+        def_sum,
+        "def_100",
+        def_100,
+        "spd",
+        spd,
+        "cRate",
+        cRate,
+        "cDmg",
+        cDmg,
+        "resist",
+        resist,
+        "acc",
+        acc,
+        "set"
+      );
     },
   },
 };
--- src/components/r-a4.vue
+++ src/components/r-a4.vue
@@ -3,26 +3,21 @@
     <div class="a4">
       <div class="a4_main power">
         <div class="power_person">
-          <h2>{{ $store.state.lang.rank }}Rank</h2>
-          <select v-model="$store.state.options[117]" class="" name="" id="">
-            <option class="gold" value="3">legend</option>
-            <option class="purpur" value="2">epic</option>
-            <option class="blue" value="1">rar</option>
-            <option class="green" value="0">top</option>
-            <option class="white" value="0">low</option>
+          <h2>{{ $store.state.lang.rank }}</h2>
+          <select
+            v-model="$store.state.options[117]"
+            :class="clas"
+            name=""
+            id=""
+          >
+            <option class="gold" value="3">Legendary</option>
+            <option class="purpur" value="2">Epic</option>
+            <option class="blue" value="1">Rare</option>
+            <option class="green" value="0">Uncommon</option>
+            <option class="white" value="-1">Common</option>
           </select>
         </div>
-        <div class="power_empower">
-          <h2>{{ $store.state.lang.empower }}</h2>
-          <select v-model="$store.state.options[118]" name="" id="">
-            <option value="0">0</option>
-            <option value="1">+1</option>
-            <option value="2">+2</option>
-            <option value="3">+3</option>
-            <option value="4">+4</option>
-          </select>
-        </div>
-        <div class="power_faction">
+        <div v-if="+$store.state.options[117] > 0" class="power_faction">
           <h2>{{ $store.state.lang.faction }}</h2>
           <select v-model="$store.state.options[119]" name="" id="">
             <option value="0">0</option>
@@ -33,13 +28,51 @@
             <option value="10">10/10</option>
           </select>
         </div>
+        <div v-if="$store.state.options[117] == 3" class="power_empower">
+          <h2>{{ $store.state.lang.empower }}</h2>
+          <select v-model="$store.state.options[118]" name="" id="">
+            <option value="0">0</option>
+            <option value="1">+1</option>
+            <option value="2">+2</option>
+            <option value="3">+3</option>
+            <option value="4">+4</option>
+          </select>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      clas: `gold`,
+    };
+  },
+  beforeMount() {
+    this.clas = this.$store.state.options[117];
+  },
+  beforeUpdate() {
+    switch (this.$store.state.options[117]) {
+      case "3":
+        this.clas = "gold";
+        break;
+      case "2":
+        this.clas = "purpur";
+        break;
+      case "1":
+        this.clas = "blue";
+        break;
+      case "0":
+        this.clas = "green";
+        break;
+      case "-1":
+        this.clas = "white";
+        break;
+    }
+  },
+};
 </script>
 
 <style lang="scss">
@@ -60,17 +93,18 @@ export default {};
   width: 20%;
 }
 .gold {
-  color: gold;
+  background: orange;
 }
 .purpur {
-  color: purple;
+  background: purple;
 }
 .blue {
-  color: blue;
+  background: blue;
 }
 .green {
-  color: green;
+  background: green;
 }
 .white {
+  background: grey;
 }
 </style>
